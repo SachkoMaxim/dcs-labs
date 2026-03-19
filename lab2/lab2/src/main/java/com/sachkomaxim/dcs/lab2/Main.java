0548ba46dd89c02b2568ccd67d2264fccf104849
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2026-03-19 15:14:56 +0200

    lab2: added program logic

---
+++ lab2/lab2/src/main/java/com/sachkomaxim/dcs/lab2/Main.java
@@ -0,0 +1,9 @@
+package com.sachkomaxim.dcs.lab2;
+
+public class Main {
+    public static void main(String[] args) {
+        //Data.generateContent(); // Для порівняльних тестувань з увімкненим та вимкненим TSX
+
+        Program.main(args);
+    }
+}
